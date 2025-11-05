83079c34c7075b4c148b2b239254f475f27a9eb3
takuma <takuma.inagaki@rakus-partners.co.jp>
2025-11-05 15:00:56 +0900

    Add

--- src/tests/test_lambda_functions.py
+++ src/tests/test_lambda_functions.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import patch, MagicMock
 import json
 import os
-from src.lambda.lambda_test import get_youtube_api_key, get_channel, get_video, lambda_handler
+# from src.lambda.lambda_test import get_youtube_api_key, get_channel, get_video, lambda_handler
 
 # チャンネル情報APIレスポンスのモック
 MOCK_CHANNEL_RESPONSE = {
